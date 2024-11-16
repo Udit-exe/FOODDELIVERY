fffb6f033dc0fe14c57f1d20ebc344ba10738e1d
Udit Agrawal <143325564+Udit-exe@users.noreply.github.com>
2024-11-16 14:12:36 +0000

    Minor Changes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './Navbar.css'
+import {assets} from '../../assets/assets.js'
 
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-
+      <img src={assets.logo} className='logo'></img>
+      <ul className="navbar-">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile App</li>
+        <li>Contact Us</li>
+      </ul>
+       <div className="navbar">
+        <img src={assets.search_icon} alt="" />
+       </div>
     </div>
   )
 }

a0613211b5be21fc158ff45f29d6977673b8a629
Udit Agrawal <143325564+Udit-exe@users.noreply.github.com>
2024-12-16 15:18:08 +0000

    Added backend configuration

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,22 @@
 import express from 'express'
 import cors from 'cors'
+
+
+
+// app config
+const app =  express()
+const PORT = 4000;
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req , res)=>{
+    res.send("API Working")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server started on http://localhost:${PORT}`)
+})
+
+// mongodb+srv://iamuditagrawal:10691069@cluster0.74an9.mongodb.net/?

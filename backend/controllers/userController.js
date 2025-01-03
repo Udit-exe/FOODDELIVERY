69078b444606a5eca13fd5b65d90679bcfe153cd
Udit Agrawal <143325564+Udit-exe@users.noreply.github.com>
2025-01-03 05:59:37 +0000

    Bug fixes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-import userModel from "../models/userModel";
+import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken"
 import bcrypt from 'bcrypt'
 import validator from 'validator'
@@ -47,7 +47,8 @@ const registerUser = async(req, res) => {
         res.json({success:true,token});
 
     } catch (error) {
-        
+        console.log(error);
+        res.json({success:false,message:"Error"})
     }
 }
 

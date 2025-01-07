d28e791c6d2af910befa5b0d1392897a3e4964d5
Udit Agrawal <143325564+Udit-exe@users.noreply.github.com>
2025-01-07 04:49:51 +0000

    made small changes

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,20 @@
+import userModel from "../models/userModel.js"
+
+//add items to users cart
+const addToCart = async (req, res) => {
+    
+}
+
+
+//remove items from users cart
+const removeFromCart = async (req, res) => {
+    
+}
+
+
+//fetch user cart data
+const getCart = async (req, res) => {
+    
+}
+
+export {addToCart, removeFromCart, getCart}
